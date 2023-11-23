872ceb1e17382ed2f15e793cc7315165213ea8b4
saadghazal <saad.m.ghazal@icloud.com>
2023-11-23 18:07:53 +0300

    create user route initially created

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,7 +1,28 @@
-module.exports = {
-    updateUser: async (req, res) => {
-
-    },
+const User = require("../models/User")
 
 
+module.exports  = {
+    createUser: async (req,res)=>{
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        })
+
+        try{
+            const savedUser = await  newUser.save()
+
+            res.status(201).json(savedUser)
+        }
+        catch (e){
+            res.status(500).json(e)
+        }
+    }
 }
+// module.exports = {
+//     updateUser: async (req, res) => {
+//
+//     },
+//
+//
+// }
