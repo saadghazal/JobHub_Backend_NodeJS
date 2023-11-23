b89674f964332078af6edcab47df4152e598ff70
saadghazal <saad.m.ghazal@icloud.com>
2023-11-23 18:14:55 +0300

    use auth route in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
 dotenv.config();
 // process.env.VARIABLE_NAME
 
@@ -11,5 +12,9 @@ mongoose.connect(process.env.MONGO_DB_URL).then(result => {
     console.log("NOT CONNECTED")
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.json())
+app.use("/api",authRoute)
+// localhost:5001/api/register
+
+
 app.listen(process.env.PORT || 5002, () => console.log(`Example app listening on port ${process.env.PORT}!`))
