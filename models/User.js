ea4d04e4cdc14be2e53398d868c45ebda7683c9b
saadghazal <saad.m.ghazal@icloud.com>
2023-11-23 15:36:15 +0300

    user schema created

--- models/User.js
+++ models/User.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+
+const  UserSchema = mongoose.Schema(
+    {
+        username:{type: String ,required: true,unique: true},
+        email: {type: String ,required: true,unique: true},
+        password: {type: String ,required: true},
+        location: {type: String ,required: false},
+        isAdmin: {type: Boolean,default:false},
+        isAgent: {type: Boolean,default: false},
+        skills: {type: Array},
+        profile_image: {
+            type: String,
+            default: "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg",
+        }
+    }
+);
+
+module.exports = mongoose.model("User",UserSchema)
