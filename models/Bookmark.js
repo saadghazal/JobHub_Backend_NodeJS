7d34ee2550a8ec80655588e7be1dbfec388e9e9a
saadghazal <saad.m.ghazal@icloud.com>
2023-11-23 15:52:50 +0300

    bookmark schema created

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 
+const BookmarkSchema = new mongoose.Schema(
+    {
+        job: {type: String, required: true},
+        user_id: {type: String,required: true},
+        title: {type: String, required: true},
+        company:{type: String,required:true},
+        location: {type: String, required: true},
+        image_url: {type: String ,required:true},
+    },{timestamps: true}
+)
+
+module.exports = mongoose.model("Bookmark",BookmarkSchema)
