0d47fc6979199602f0a7e57b47577c6e37fbd549
saadghazal <saad.m.ghazal@icloud.com>
2023-11-30 16:12:11 +0300

    GET All  Jobs Route Created

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -48,6 +48,16 @@ module.exports = {
         }
     },
 
+    getAllJobs: async (req,res)=>{
+        try {
+            const jobs = await Job.find()
+            res.status(200).json(jobs)
+
+        }catch (error){
+            res.status(500).json(error)
+        }
+    },
+
 
 
 } 
--- routes/job.js
+++ routes/job.js
@@ -15,8 +15,8 @@ router.delete("/:id",verifyAndAdmin,jobController.deleteJob)
 // GET JOB
 router.get("/:id",jobController.getJob)
 
-// // GET All JOBS
-// router.get("/",jobController.getAllJobs)
+// GET All JOBS
+router.get("/",jobController.getAllJobs)
 
 // SEARCH JOBS
 // router.get("/search/:key",jobController.searchJobs)
