75a722bd1bb7df921f537e804d5ad6f67ec34c10
saadghazal <saad.m.ghazal@icloud.com>
2023-12-02 00:20:06 +0300

    Update Bookmark Schema Structure

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 
 const BookmarkSchema = new mongoose.Schema(
     {
-        job: {type: String, required: true},
+        job: {type: mongoose.Schema.Types.ObjectId, ref: "Job"},
         user_id: {type: String,required: true},
-        title: {type: String, required: true},
-        company:{type: String,required:true},
-        location: {type: String, required: true},
-        image_url: {type: String ,required:true},
+       
     },{timestamps: true}
 )
 
