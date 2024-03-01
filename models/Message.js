34118186e1d2f6fbc11d3cadb1ed63bfcc7c9f09
saadghazal <saad.m.ghazal@icloud.com>
2024-03-01 15:16:51 +0300

    Modify Message Schema

--- models/Message.js
+++ models/Message.js
@@ -4,7 +4,13 @@ const messageSchema = new mongoose.Schema({
     sender: {type: mongoose.Schema.ObjectId,ref:'User'},
     content: {type: String},
     receiver: {type: mongoose.Schema.ObjectId,ref:'User'},
-    chat: {type: mongoose.Schema.ObjectId,ref:'Chat'}
+    chat: {type: mongoose.Schema.ObjectId,ref:'Chat'},
+    readBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
 },{timestamps: true})
 
 
