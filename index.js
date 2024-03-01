7ba62720704ba9d8f7bc75f69158d8f8922f8268
saadghazal <saad.m.ghazal@icloud.com>
2024-03-01 16:40:25 +0300

    Setup Out Socket

--- index.js
+++ index.js
@@ -35,4 +35,67 @@ app.use('/api/messages',messageRoute)
 
 
 
-app.listen(process.env.PORT || 5002, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+const server = app.listen(process.env.PORT || 5002, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+
+
+const io = require('socket.io')(server,{
+    pingTimout: 60000,
+    cors: {
+        // origin: "http:localhost:5001"
+        // hosted server
+        origin:  "https://jobhubbackendnodejs-production.up.railway.app/"
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log('Connected To Socket')
+    socket.on('setup',(userId)=> {
+        socket.join(userId)
+        socket.broadcast.emit('online-user',userId)
+        console.log(userId)
+    })
+
+    socket.on('typing',(room)=>{
+        console.log('typing')
+        console.log(room)
+        socket.to(room).emit('typing',room)
+    })
+
+    socket.on('stop typing',(room)=>{
+        console.log('stop typing')
+        console.log(room)
+        socket.to(room).emit('stop typing',room)
+    })
+
+    socket.on('join chat',(room)=>{
+        socket.join(room)
+        console.log('User Joined: '+room)
+    })
+
+    socket.on('new message',(newMessageString)=>{
+        let chat = newMessageString.chat;
+        let room = chat._id
+        let sender = newMessageString.sender
+        let senderId = sender._id
+
+        if(!sender || !senderId){
+            console.log('Sender not defined')
+            return
+        }
+        console.log(senderId + "message sender")
+        const users = chat.users;
+
+
+        if(!users){
+            console.log('Users not defined')
+            return;
+        }
+        socket.to(room).emit('message received',newMessageReceived)
+        socket.to(room).emit('message sent',"New Message")
+    })
+
+    socket.off('setup',(userId)=>{
+        console.log('User offline')
+        socket.leave(userId)
+    })
+})
